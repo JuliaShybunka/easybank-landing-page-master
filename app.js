f3ee093bad3a9155266d8eecb2fae0464384fa0d
Julia Shybunka <julishybunka@gmail.com>
2020-07-07 19:51:09 +0300

    Create header HTML structure

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.set(express.static('public'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
 
 
 app.listen(3000, function() {
